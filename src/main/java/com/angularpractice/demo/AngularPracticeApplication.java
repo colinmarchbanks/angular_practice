82bab39497354d3fb0adfc77d91da9ef4752f961
colinmarchbanks <marchbanks.colin@gmail.com>
2020-03-10 12:39:12 -0500

    First angular example from w3

---
+++ src/main/java/com/angularpractice/demo/AngularPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.angularpractice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/angularpractice/demo/AngularPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularpractice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
